920f1ab7680d5b7303f9bd9b03622c3c322c4ae5
faraz-ghamsari <ghamsari.frontend@gmail.com>
2022-11-20 19:16:37 +0330

    uploading postModal

--- components/PostModal/PostModal.jsx
+++ components/PostModal/PostModal.jsx
@@ -2,6 +2,19 @@ import styles from './PostModal.module.css'
 import {BiXCircle} from "react-icons/bi";
 import Image from 'next/image';
 
+const comments= [
+    {c:'hello its comment test i hope you like ittttttttttttttttttttttttttttttttttttttttttttt'},
+    {c:'hello its comment test i hope you like it'},
+    {c:'hello its comment test i hope you like it'},
+    {c:'hello its comment test i hope you like it'},
+    {c:'hello its comment test i hope you like it,hello its comment test i hope you like it,hello its comment test i hope you like it'},
+    {c:'hello its comment test i hope you like it'},
+    {c:'hello its comment test i hope you like it'},
+    {c:'hello its comment test i hope you like it'},
+    {c:'hello its comment test i hope you like it'},
+    {c:'hello its comment test i hope you like it'},
+]
+
 const myLoader = ({ src, width, quality }) => {
     return `${src}?w=${width}&q=${quality || 75}`
 }
@@ -21,9 +34,28 @@ const PostModal = ({data,show,modalContent}) => {
                                     <Image loader={myLoader} src={d.image} alt='modal image' width={100} height={100}/>
                                 </div>
                                 <div className={styles.modalInfo}>
-                                     <div>{d.name}</div> 
-                                     <span>تهران منطقه ۴ شهر تهران محله پاسداران - ضرابخانه پاسداران گلستان ۶</span>
+                                    <div className={styles.modalHeader}>
+                                        <div>{d.name}</div> 
+                                        <span>تهران منطقه ۴ شهر تهران محله پاسداران - ضرابخانه پاسداران گلستان ۶,</span>
+                                    </div>
+                                    <div className={styles.comments}>
+                                        {comments.map((c)=>(
+                                            <div className={styles.comment}>
+                                                <div className={styles.commentImg}>
+                                                    <Image loader={myLoader} src={d.image} alt='modal image' width={100} height={100} /> 
+                                                </div>
+                                                <div className={styles.commentMsg}>{c.c}</div> 
+                                            </div>
+                                            
+                                        ))}
+                                    </div>
+                                    <form className={styles.addComment}>
+                                        <input type="text" />
+                                        <button>ارسال</button>
+                                    </form>
+                                
                                 </div>
+                                
                             </>
                         ))}
                         <span className={styles.closeBtn} onClick={modalContent}>
